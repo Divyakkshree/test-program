5cfe23e45757e3d2bfffb18eed72b2e92d9668e0
Divyakkshree <divyakkshree@gmail.com>
2025-07-29 11:02:45 +0530

    First level completed
    Created First level, when playing game all cards are matching so then game win panel displayed

--- MyTestProgram/Assets/Scripts/GameManager.cs
+++ MyTestProgram/Assets/Scripts/GameManager.cs
@@ -2,19 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
 public class GameManager : MonoBehaviour
 {
 
-
     public List<Button> btns = new List<Button>();
+
     [SerializeField]
     private Sprite bgImage;
+
+    public Sprite[] Cards;
+
+    public List<Sprite> gameCards = new List<Sprite>();
+    
+    private bool firstMatch, secondMatch;
+
+    private int countMatches;
+    private int countCorrectMatches;
+    private int Matches;
+
+    private int firstMatchIndex, secondMatchIndex;
+
+    private string firstMatchCard, secondMatchCard;
+    
+    public GameObject gameWinPanel;
 
      void Start()
     {
         GetButtons();
         AddListeners();
+        AddCards();
+        Shuffle(gameCards);
+        Matches=gameCards.Count/2;
+
         }
+
+
+     private void Awake()
+    {
+        Cards =Resources.LoadAll<Sprite>("Images/Cards");
+    }
      void GetButtons()
     {
         GameObject[] objects = GameObject.FindGameObjectsWithTag("CardBtn");
@@ -23,6 +52,21 @@ public class GameManager : MonoBehaviour
         {
             btns.Add(objects[i].GetComponent<Button>());
             btns[i].image.sprite=bgImage;
+        }
+    }
+      void AddCards()
+    {
+        int countcard = btns.Count;
+        int index=0;
+
+        for(int i = 0; i < countcard; i++)
+        {
+            if(index == countcard/2)
+            {
+                index = 0;
+            }
+            gameCards.Add(Cards[index]);
+            index++;
         }
     }
      void AddListeners()
@@ -36,11 +80,79 @@ public class GameManager : MonoBehaviour
     {
         string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
         print("hey"+name);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+         if(!firstMatch)
+        {
+            firstMatch=true;
+            firstMatchIndex=int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            firstMatchCard=gameCards[firstMatchIndex].name;
+            btns[firstMatchIndex].image.sprite=gameCards[firstMatchIndex];
+        }
+        else if(!secondMatch){
         
+            secondMatch=true;
+            secondMatchIndex=int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+                        secondMatchCard=gameCards[secondMatchIndex].name;
+
+            btns[secondMatchIndex].image.sprite=gameCards[secondMatchIndex];
+            if(firstMatchCard==secondMatchCard)
+            {
+                print("card match");
+            }
+            else{
+                print("card not match");
+            }
+                        StartCoroutine(checkCardMatch());
+
+                      //  Matches++;
+                      }
     }
+     IEnumerator checkCardMatch()
+        {
+            yield return new WaitForSeconds(0.5f);
+            if(firstMatchCard==secondMatchCard)
+            {
+           // btns[firstMatchIndex].interactable=false;
+           // btns[secondMatchIndex].interactable=false;
+                GameFinish();
+            }
+            else
+            {
+                btns[firstMatchIndex].image.sprite=bgImage;
+                btns[secondMatchIndex].image.sprite=bgImage;
+            }
+             firstMatch=secondMatch=false;
+
+        }
+   public void GameFinish()
+   {
+        countCorrectMatches++;
+        if(countCorrectMatches == Matches)
+        {
+            print("game win");
+            gameWinPanel.SetActive(true);
+            print("took"+countCorrectMatches+ countMatches);
+        }
+   }
+   public void NextBtn()
+   {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+   }
+   public void RetryBtn()
+   {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+   }
+   void Shuffle(List<Sprite> list)
+
+   {
+    for (int i = 0; i < list.Count; i++)
+    {
+        Sprite temp =list[i];
+        int randomIndex =Random.Range(i,list.Count);
+        list[i]=list[randomIndex];
+        list[randomIndex]=temp;
+    }
+   }
+
 }
